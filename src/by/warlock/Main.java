8f8e3ad4437286c6991da9007591ee9f243cd970
navi1603 <ukrainec1603@gmail.com>
2026-01-17 22:35:56 +0300

    Добавлены методы, создана структура классов.

--- src/by/warlock/Main.java
+++ src/by/warlock/Main.java
@@ -2,6 +2,21 @@ package by.warlock;
 
 public class Main {
     static void main(String[] args) {
+        PostCell[] postCells = {
+                new PostCell(new Dimensions(10.5, 5, 5), ShipmentStatus.WORK),
+                new PostCell(new Dimensions(10.5, 5, 5), ShipmentStatus.WORK),
+                new PostCell(new Dimensions(10.5, 5, 5), ShipmentStatus.WORK),
+                new PostCell(new Dimensions(10.5, 5, 5), ShipmentStatus.WORK),
+                new PostCell(new Dimensions(10.5, 5, 5), ShipmentStatus.WORK)
+        };
+        Postamat postamat = new Postamat(postCells);
+
+        Shipment shipment1 = new Shipment(new Dimensions(5.5, 3, 2), 0.5, "From AliExpress");
+        Shipment shipment2 = new Shipment(new Dimensions(5.5, 3, 2), 0.5, "From AliExpress");
+        Shipment shipment3 = new Shipment(new Dimensions(12, 6, 2), 0.5, "From AliExpress");
+        Shipment shipment4 = new Shipment(new Dimensions(13, 5, 7), 0.5, "From AliExpress");
+
+
 
     }
 }
--- src/by/warlock/Postamat.java
+++ src/by/warlock/Postamat.java
@@ -6,15 +6,19 @@ public class Postamat {
 
 
     public Postamat() {
-
         postCells = new PostCell[10];
     }
 
-    public void putShipment(Shipment shipment) {
-        for (PostCell postCell : postCells){
-            if(postCell == null){
-        }
+    public Postamat(PostCell[] postCells) {
+        this.postCells = postCells;
+    }
 
+    public void putShipment(Shipment shipment) {
+        for (int i = 0; i < postCells.length; i++) {
+            if (postCells[i] == null) {
+                postCells[i].putShipment(shipment);
+                break;
+            }
         }
     }
 
